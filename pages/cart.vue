690c20fb395494a9b718ab6be6a80a349acdb015
Patryk Jachowski <patryk.jachowski@gmail.com>
2025-09-15 11:15:26 +0200

    feat: add responsive cart total summary indicator

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,9 +1,42 @@
 <template>
-  <ProductsList
-    v-if="products.length"
-    :products="products"
-    mode="remove"
-  />
+  <div v-if="products.length">
+    <ProductsList
+      :products="products"
+      mode="remove"
+    />
+
+    <div
+      class="mt-6 sm:mt-8 p-4 sm:p-6 bg-gray-50 rounded-lg border border-gray-200 mx-4 sm:mx-0"
+    >
+      <div
+        class="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4"
+      >
+        <div class="text-center sm:text-left">
+          <h3 class="text-lg font-semibold text-gray-900">
+            Podsumowanie koszyka
+          </h3>
+          <p class="text-sm text-gray-600">
+            {{ totalItems }}
+            {{
+              totalItems === 1
+                ? "produkt"
+                : totalItems < 5
+                  ? "produkty"
+                  : "produktów"
+            }}
+          </p>
+        </div>
+        <div class="text-center sm:text-right">
+          <div class="text-xl sm:text-2xl font-bold text-gray-900">
+            {{ formatCurrency(totalPrice) }}
+          </div>
+          <div class="text-sm text-gray-600">
+            Łączna wartość
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div
     v-else
     class="flex flex-col gap-6 items-center h-[calc(100vh-64px)] justify-center"
@@ -30,7 +63,10 @@
 
 <script setup lang="ts">
 import { useCartStore } from '~/stores/cart'
+import { useFormatCurrency } from '~/utils/use-format-currency'
 
 const store = useCartStore()
 const products = store.items
+const { totalItems, totalPrice } = storeToRefs(store)
+const formatCurrency = useFormatCurrency
 </script>
